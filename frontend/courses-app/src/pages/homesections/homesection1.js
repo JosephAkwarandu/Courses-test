c8255a12f38e74d470a758d8989903a01f369c06
Joseph <monkeybarten@gmail.com>
2023-09-22 20:58:17 +0100

    fifth commit

--- frontend/courses-app/src/pages/homesections/homesection1.js
+++ frontend/courses-app/src/pages/homesections/homesection1.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import {Icon} from '@iconify/react'
 import img1 from '../../images/img1.jpg'
 
@@ -12,9 +12,9 @@ export default function Homesection1() {
         <p><span>New</span> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
         <h1>Find your <span className='courseText'>Course</span> and get certified</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, in?</p>
-        <div className='buttonContainer'>
-          <NavLink to='courses' className='btn1'>Get Started</NavLink>
-          <NavLink className='btn2'> <p>Play video <Icon icon="octicon:play-24" /></p></NavLink>
+        <div  className='buttonContainer'>
+          <Link to='courses' className='btn1'>Get Started</Link>
+          <Link className='btn2'> <p>Play video <Icon icon="octicon:play-24" /></p></Link>
         </div>
       </div>
       <div className='imgSide'> 
